49a9c5d396fb6d152531499a52980857a08987e5
laughingman <webmaster@laughingman.dev>
2023-01-15 10:11:44 +0100

    example structure for server as object

--- src/types/types.go
+++ src/types/types.go
@@ -1,8 +1,37 @@
 package types
 
+import "sync"
+
 // - - - - - - - - - - - - - - - - - - - - - - - - - -
 // - - - - - - - - STORAGE STRUCTS - - - - - - - - - -
 // - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+type Storage struct {
+	Mutex struct {
+		EntityType sync.RWMutex
+		Entity     sync.RWMutex
+		Relation   sync.RWMutex
+	}
+	Relations struct {
+		DirectedIndex   map[int]map[int]map[int]map[int]*StorageRelation
+		DirectedRIndex  map[int]map[int]map[int]map[int]*StorageRelation
+		UndirectedIndex map[int]struct {
+			Alpha   [2]int
+			Beta    [2]int
+			Pointer *StorageRelation
+		}
+		Data map[int]StorageRelation
+	}
+	Entities struct {
+		IDMax map[int]int
+		Index map[int]map[int]*StorageEntity
+		Data  map[int]StorageEntity
+	}
+	Types struct {
+		IDMax int
+		Types map[int]string
+	}
+}
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - -
 // StorageEntity struct
